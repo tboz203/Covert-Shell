a424ea111684506895b3d9c26f4fd8651601e0d8
tboz203 <tboz203@gmail.com>
2014-02-21 19:47:41 -0600

    working on making a portable version

--- test.py
+++ test.py
@@ -3,34 +3,30 @@
 from __future__ import print_function
 
 import sys
-import io
-import threading
-from subprocess import Popen, PIPE
+from random import randint
+from time import sleep
+from socket import socket, AF_INET, SOCK_RAW, IPPROTO_ICMP
 
-def reader(so):
-    try:
-        print('this is fucking bullshit')
-    except Exception as e:
-        print('[-] Reader says:', e)
+from checksum import checksum
 
-def writer(si):
-    try:
-        print('wtf')
-    except Exception as e:
-        print('[-] Writer says:', e)
+s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)
 
+tobytes = lambda x: bytearray.fromhex('{:04x}'.format(x))
+tohex = lambda x: ' '.join(['{:02x}'.format(item) for item in x])
 
-write = threading.Thread(target=writer, args=['asdf'])
-read = threading.Thread(target=reader, args=['lolol'])
+mesg = bytearray('this is my message')
+type_code = bytearray('\x08\x00')
+# ident = tobytes(randint(0, 65535))
+ident = tobytes(0)
+fakecheck = bytearray('\x00\x00')
 
-print('[!] threads created')
+for i in range(256):
+    seq = tobytes(i)
+    realcheck = tobytes(checksum(type_code + fakecheck + ident + seq + mesg))
+    data  = type_code + realcheck + ident + seq + mesg
 
-write.start()
-read.start()
+    s.sendto(data, ('localhost', 0))
 
-print('[!] threads started')
-
-write.join()
-read.join()
-
-print('[!] threads joined')
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    sleep(1)
